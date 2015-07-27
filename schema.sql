64e7ee6507c51fb1e7d7fd3dd5a6bd4039a8607c
Corey Cossentino <corey@cossentino.com>
2015-07-27 16:28:30 -0400

    some schema changes

--- schema.sql
+++ schema.sql
@@ -12,6 +12,12 @@ ON INSERT TO soccerdome.teams
 WHERE EXISTS (SELECT 1 FROM soccerdome.teams WHERE team_name = NEW.team_name)
 DO INSTEAD NOTHING;
 
+CREATE TABLE soccerdome.team_misspellings (
+  team_id INTEGER PRIMARY KEY,
+  team_name TEXT UNIQUE NOT NULL,
+  FOREIGN KEY (team_id) REFERENCES soccerdome.teams(team_id)
+);
+
 CREATE TABLE soccerdome.leagues (
   league_id INTEGER PRIMARY KEY,
   league_name TEXT NOT NULL
@@ -39,15 +45,15 @@ CREATE TABLE soccerdome.games (
 CREATE INDEX ON soccerdome.games(home_team);
 CREATE INDEX ON soccerdome.games(away_team);
 CREATE INDEX ON soccerdome.games(game_date);
-CREATE UNIQUE INDEX ON soccerdome.games(game_date, home_team, away_team);
+CREATE UNIQUE INDEX ON soccerdome.games(date_trunc('day', game_date), home_team, away_team);
 
 CREATE RULE games_update_duplicates AS
 ON INSERT TO soccerdome.games
-WHERE EXISTS (SELECT 1 FROM soccerdome.games WHERE game_date = NEW.game_date AND home_team = NEW.home_team AND away_team = NEW.away_team)
+WHERE EXISTS (SELECT 1 FROM soccerdome.games WHERE date_trunc('day', game_date) = date_trunc('day', NEW.game_date) AND home_team = NEW.home_team AND away_team = NEW.away_team)
 DO INSTEAD
 UPDATE soccerdome.games
   SET complete = NEW.complete, home_score = NEW.home_score, away_score = NEW.away_score
-  WHERE game_date = NEW.game_date AND home_team = NEW.home_team AND away_team = NEW.away_team;
+  WHERE date_trunc('day', game_date) = date_trunc('day', NEW.game_date) AND home_team = NEW.home_team AND away_team = NEW.away_team;
 
 CREATE TABLE soccerdome.elo_ratings (
   rating_id SERIAL PRIMARY KEY,
